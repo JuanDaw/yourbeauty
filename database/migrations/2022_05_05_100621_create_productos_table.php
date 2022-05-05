fbcfea3533dac2d48b35764a909b48e881980fa7
Juan ANtonio Villoria del Moral <juvi.13.13@gmail.com>
2022-05-05 12:31:13 +0200

    Migracines iniciales

---
+++ database/migrations/2022_05_05_100621_create_productos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->text('descripcion');
+            $table->foreignId('categoria_id')->constrained('categorias');
+            $table->string('marca')->nullable();
+            $table->string('link');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+};
